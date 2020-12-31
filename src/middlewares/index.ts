6b3e028579ca36d2d6a144c980991ba1a1034e4e
JVSC <joaovictor.clementino@gmail.com>
2020-12-30 23:16:18 -0300

    clear cookies on logout

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -11,12 +11,12 @@ export async function authenticate(
     if (!token) return res.status(401);
 
     const u: any = await verify(token, process.env.TOKEN_SECRET as string);
-    if (!u.auth) return res.status(401).json({});
+    if (!u.auth) return res.clearCookie("session_id").status(401).json({});
     req.res.locals.token = u;
     req.res.locals.id = u.id;
     next();
   } catch (err) {
-    res.status(401).json({});
+    res.clearCookie("session_id").status(401).json({});
     throw err;
   }
 }
@@ -31,12 +31,13 @@ export async function authUserGameSession(
     if (!token) return res.status(301).redirect("/login");
 
     const u: any = await verify(token, process.env.TOKEN_SECRET as string);
-    if (!u.auth) return res.status(301).redirect("/login");
+    if (!u.auth)
+      return res.clearCookie("session_id").status(301).redirect("/login");
     req.res.locals.token = u;
     req.res.locals.id = u.id;
     next();
   } catch (err) {
-    return res.status(301).redirect("/login");
+    return res.clearCookie("session_id").status(301).redirect("/login");
   }
 }
 
@@ -51,33 +52,9 @@ export async function authenticateAdmin(
 
     const u: any = await verify(token, process.env.TOKEN_SECRET as string);
     if (u.authLevel < 100)
-      return res.status(301).redirect(process.env.ROOT_URL);
+      return res.clearCookie("session_id").status(301).redirect(process.env.ROOT_URL);
     else next();
   } catch (err) {
-    return res.status(301).redirect(process.env.ROOT_URL);
+    return res.clearCookie("session_id").status(301).redirect(process.env.ROOT_URL);
   }
 }
-
-const generateGuest = () => {
-  return {
-    rank: { authLevel: -1, rankName: "Guest" },
-    id: Math.floor(Math.random() * 1000000 + 1) * -1,
-    avatar: "1.jpg",
-    coins: 0,
-    username: `GUEST-${Math.random()
-      .toString(36)
-      .replace(/[^a-z]+/g, "")
-      .substr(0, 2)}`,
-    season: {
-      elo: 1000,
-      wins: 0,
-      losses: 0,
-      streak: 0,
-      exp: 0,
-      maxStreak: 0,
-      seasonRank: "Rookie",
-      seasonLevel: 0,
-    },
-    unlocked: [Number],
-  };
-};
