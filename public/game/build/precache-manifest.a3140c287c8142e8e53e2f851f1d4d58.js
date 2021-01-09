8da892972fda156c507b0460b58ccc1235b9fa8e
JVSC <joaovictor.clementino@gmail.com>
2021-01-09 18:32:06 -0300

    add pwa support

---
+++ public/game/build/precache-manifest.a3140c287c8142e8e53e2f851f1d4d58.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fcaa602873257b66f84",
+    "url": "/css/app.eeca190c.css"
+  },
+  {
+    "revision": "f416f30d119aff5feb2c",
+    "url": "/css/chunk-00c852f4.addb0430.css"
+  },
+  {
+    "revision": "202fd5cf76b9e14d481e",
+    "url": "/css/chunk-2a74be66.5dfa6e82.css"
+  },
+  {
+    "revision": "18f262b05b8b2c60d4b2",
+    "url": "/css/chunk-2d29ce14.470bcb24.css"
+  },
+  {
+    "revision": "ee59377ec967006a9356",
+    "url": "/css/chunk-59a9fa74.fffdb734.css"
+  },
+  {
+    "revision": "58775dfe8b4ac4585895",
+    "url": "/css/chunk-5a92e448.f9119388.css"
+  },
+  {
+    "revision": "7e8a8475a8fffe4e6b98",
+    "url": "/css/chunk-73fd817d.d2541720.css"
+  },
+  {
+    "revision": "8740f1dc9a6ae556842a",
+    "url": "/css/chunk-d8a5091a.2b52eefd.css"
+  },
+  {
+    "revision": "678b7c6ba3e7bd83b54b7c2672051236",
+    "url": "/img/battle1.678b7c6b.jpg"
+  },
+  {
+    "revision": "8f6dd7e08cde4382eab27279b71680dd",
+    "url": "/img/battle2.8f6dd7e0.jpg"
+  },
+  {
+    "revision": "20a62f65ec23a9069137500be818816d",
+    "url": "/img/bg2.20a62f65.jpg"
+  },
+  {
+    "revision": "f0853dd36eadbc724f061da5903eb8be",
+    "url": "/img/body.f0853dd3.png"
+  },
+  {
+    "revision": "56da7441e0b8d9143f2bc1b680db357d",
+    "url": "/img/body2.56da7441.png"
+  },
+  {
+    "revision": "76bf98dba303d5d570a081b00e640f46",
+    "url": "/img/coins_1.76bf98db.svg"
+  },
+  {
+    "revision": "592f4b5aa9137d920a384fe36271572f",
+    "url": "/img/goback.592f4b5a.svg"
+  },
+  {
+    "revision": "6ae20566812d7fdb66d9fc4b9842d2f4",
+    "url": "/img/interrogation.6ae20566.png"
+  },
+  {
+    "revision": "d63e84527db548a7f6b7c440941c4a39",
+    "url": "/img/loser.d63e8452.png"
+  },
+  {
+    "revision": "b843c179748f2f1b96724ef9311a7041",
+    "url": "/img/next.b843c179.svg"
+  },
+  {
+    "revision": "614f119cb7d90fb2f3fb2ba3a54591bd",
+    "url": "/img/rosterdark.614f119c.png"
+  },
+  {
+    "revision": "7c2bf824fbb73e43532af8b798de03cc",
+    "url": "/img/sa.7c2bf824.png"
+  },
+  {
+    "revision": "61246c0668c2ae23ff3f94c0f0b9bb0b",
+    "url": "/img/star.61246c06.svg"
+  },
+  {
+    "revision": "3d2c8647547c558c34c78e64b8d05c0d",
+    "url": "/img/user.3d2c8647.svg"
+  },
+  {
+    "revision": "cca21584985acc7f421a3ad4903ede4d",
+    "url": "/img/wait.cca21584.gif"
+  },
+  {
+    "revision": "fde526c9e2157fc8e8dec9a52da3af55",
+    "url": "/img/watercolor-brush.fde526c9.png"
+  },
+  {
+    "revision": "7d69349724d2409cc8f6d92e1c9bbe9c",
+    "url": "/img/winner.7d693497.png"
+  },
+  {
+    "revision": "890d19319c5f31a53d234d153d7021c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fcaa602873257b66f84",
+    "url": "/js/app.f9bf9ba0.js"
+  },
+  {
+    "revision": "f416f30d119aff5feb2c",
+    "url": "/js/chunk-00c852f4.d84e76af.js"
+  },
+  {
+    "revision": "202fd5cf76b9e14d481e",
+    "url": "/js/chunk-2a74be66.31ba4ce3.js"
+  },
+  {
+    "revision": "18f262b05b8b2c60d4b2",
+    "url": "/js/chunk-2d29ce14.413e483e.js"
+  },
+  {
+    "revision": "ee59377ec967006a9356",
+    "url": "/js/chunk-59a9fa74.6d30e967.js"
+  },
+  {
+    "revision": "58775dfe8b4ac4585895",
+    "url": "/js/chunk-5a92e448.183d22c3.js"
+  },
+  {
+    "revision": "7e8a8475a8fffe4e6b98",
+    "url": "/js/chunk-73fd817d.788bb8d8.js"
+  },
+  {
+    "revision": "8740f1dc9a6ae556842a",
+    "url": "/js/chunk-d8a5091a.a85edc0e.js"
+  },
+  {
+    "revision": "c5c0d6fdb997e381c6f7",
+    "url": "/js/chunk-vendors.33df965d.js"
+  },
+  {
+    "revision": "3a630681469b15fd4b044c285a24c912",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
