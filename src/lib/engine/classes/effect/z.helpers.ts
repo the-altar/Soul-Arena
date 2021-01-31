2c99ba7de7db3de33aceec2333871ac3315c3ba8
JVSC <joaovictor.clementino@gmail.com>
2021-01-31 01:36:46 -0300

    None is now a neutral effect

--- src/lib/engine/classes/effect/z.helpers.ts
+++ src/lib/engine/classes/effect/z.helpers.ts
@@ -30,8 +30,6 @@ export const isHarmful = function (
       return true;
     case effectType.IncreaseTargetSkillDuration:
       return true;
-    case effectType.None:
-      return true;
     default:
       return false;
   }
